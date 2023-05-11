924363ff6feb9984e02d775a13b6902f4e45f927
Motta <125099026+Mottaqbota@users.noreply.github.com>
2023-05-11 19:57:15 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
 var divUser = document.getElementById('divUser');
-
-
 function infoUser(){
   divUser.classList.toggle('active-infos');
+  var cubo = document.getElementById('icon-infuser') 
+  cubo.classList.toggle('girar')
 };
+
+
+var loaddiv = document.getElementById('loadingPage');
+loaddiv.innerHTML = `
+  <p>Elemento Sendo carregado!!</p>
+  <button>Elemento Sendo carregado!!</button>
+`
+function loading(){
+  loaddiv.style.display = 'none';
+}
